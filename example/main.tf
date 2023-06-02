1c17773590b0579a3a8be63d47050d32166b83a9
Felipe Travi <felipe.travi@g.globo>
2023-06-02 18:42:56 -0300

    create_example

---
+++ example/main.tf
@@ -0,0 +1,24 @@
+locals {
+  project_id = "YOUR_PROJECT_ID"
+
+  dict_iam_member = {
+    "id_1" = {
+      role   = "roles/role1"
+      member = "user:user1@test.com"
+    },
+    "id_2" = {
+      role   = "roles/role2"
+      member = "group:permission1a"
+    },
+    "id_3" = {
+      role   = "roles/role3"
+      member = "serviceAccount:sa_email"
+    },
+  }
+}
+
+module "create_premission" {
+  source          = "../"
+  project_id      = local.project_id
+  dict_iam_member = local.dict_iam_member
+}
